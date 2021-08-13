7416fd5f559fc1df2937284adbd292398c73f4ab
panchen <panchen@tigerbrokers.com>
2021-08-13 20:36:53 +0800

    Initial commit

---
+++ src/main/kotlin/me/bytebeats/plugin/weather/CityService.kt
@@ -0,0 +1,13 @@
+package me.bytebeats.plugin.weather
+
+/**
+ * @Author bytebeats
+ * @Email <happychinapc@gmail.com>
+ * @Github https://github.com/bytebeats
+ * @Created on 2021/8/13 20:28
+ * @Version 1.0
+ * @Description TO-DO
+ */
+
+interface CityService {
+}
